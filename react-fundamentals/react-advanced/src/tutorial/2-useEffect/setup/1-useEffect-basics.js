9dda9382777a5bcd40d52482a2b6d10b1d5da17c
DhruvShah <dhruvshah78223@gmail.com>
2022-09-02 21:54:42 +0530

    1- useEffect basics

--- react-fundamentals/react-advanced/src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ react-fundamentals/react-advanced/src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -3,7 +3,27 @@ import React, { useState, useEffect } from 'react';
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [value, setValue] = useState(0);
+
+  //it will call the depend on the value changes 
+  useEffect(() => {
+    if (value >= 1) {
+      document.title = `New messages(${value})`;
+    }
+    console.log('call useEffect');
+    
+  },[value]);
+
+  // here empty [] bracket indicates intital render
+  useEffect(()=>{
+    console.log('intital render');
+  },[])
+
+  console.log('render component');
+  return <>
+    <h1>{value}</h1>
+    <button className='btn' onClick={() => setValue(value + 1)}>Click me</button>
+  </>;
 };
 
 export default UseEffectBasics;
