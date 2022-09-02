bf5125187d0d9d09738f2cb1e752b4ab56ed87f0
DhruvShah <dhruvshah78223@gmail.com>
2022-09-02 19:18:56 +0530

    useState Array and useState counter

--- react-fundamentals/react-advanced/src/App.js
+++ react-fundamentals/react-advanced/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import Setup from './tutorial/1-useState/setup/4-useState-object'  
+import Final from './tutorial/1-useState/final/3-useState-array'  
 // import Final from './tutorial/1-useState/final/2-useState-basics'
 
 function App() {
   return (
     <div className='container'>
-      <Setup/>
+      <Final/>
       {/* <Final/> */}
     </div>
   )
