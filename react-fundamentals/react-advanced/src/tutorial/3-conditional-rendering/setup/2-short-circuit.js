0fb7c31e0e12956c8bd998149b1dc272dfa82c45
DhruvShah <dhruvshah78223@gmail.com>
2022-09-03 16:33:01 +0530

    2 - sort-circuit javscript

--- react-fundamentals/react-advanced/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ react-fundamentals/react-advanced/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -3,10 +3,24 @@ import React, { useState } from 'react';
 // ternary operator
 
 const ShortCircuit = () => {
+  const [text,setText]=useState('');
+  const [isError,setIsError]=useState(false);
   // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+  // const secondValue = text && 'hello world2';
+  // console.log(secondValue);
+  
 
-  return <h2>short circuit</h2>;
+  return <>
+  {/* {firstValue}
+  {secondValue} */}
+  <h1>{text || 'dhruv shah'}</h1>
+  <button className='btn' onClick={()=>setIsError(!isError)}>toggle error</button>
+  {text  &&  <h1>Hello World</h1>}
+  {isError  &&  <h1>Error Showing....</h1>}
+  {isError ? <p>there is an error...</p> : <div>
+    <h2>there is no error</h2>
+  </div> }
+  </>
 };
 
 export default ShortCircuit;
